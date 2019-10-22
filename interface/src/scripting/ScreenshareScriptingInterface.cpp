cdbb9488eb14806732461ed9ff8beda797881f6d
milad <milad@miladnazeri.com>
2019-10-22 09:46:12 -0700

    working-ish api

---
+++ interface/src/scripting/ScreenshareScriptingInterface.cpp
@@ -0,0 +1,47 @@
+#include "ScreenshareScriptingInterface.h"
+#include <QProcess>
+#include <QThread>
+#include <QDesktopServices>
+#include <QUrl>
+
+ScreenshareScriptingInterface::ScreenshareScriptingInterface(){};
+
+void ScreenshareScriptingInterface::startScreenshare() {
+    if (QThread::currentThread() != thread()) {
+        // I added this because it said I can't start a process on a different thread 
+        QMetaObject::invokeMethod(this, "startScreenshare");
+        return;
+    }
+
+    qDebug() << "\n\n TESTING SCREENSHARE OPEN \n\n" + SCREENSHARE_APPLICATION;
+    QStringList arguments;
+    arguments << "test";
+    /*
+    // attempt 1
+    QProcess* process = new QProcess(this);
+    // I tried both of these
+    process->start("C:\hifi\hifi\screenshare\screenshare-win32-x64\screenshare.exe", arguments);
+    process->start(SCREENSHARE_APPLICATION, arguments);
+
+    // None of these print either
+    connect(process, &QProcess::errorOccurred,
+            [=](QProcess::ProcessError error) { qDebug() << "error enum val = " << error << endl; });
+    connect(process, &QProcess::started, [=]() { qDebug() << "PROCESS STARTED"; });
+    connect(process, &QProcess::stateChanged,
+            [=](QProcess::ProcessState newState) { qDebug() << "process state" << newState; });
+    */
+
+
+    /*
+    // attempt 2
+    // Another method I saw, but also doesn't do anything
+    QProcess::startDetached(SCREENSHARE_APPLICATION, arguments);
+    */
+    
+
+    // attempt 3, this one worked well, but with a popup asking if I am sure I want to open this url
+    // I don't think we can pass arguments in this way either so I'd say a no go.  
+    // QDesktopServices::openUrl(QUrl(SCREENSHARE_APPLICATION));
+    
+    // attempt 4
+};
---
+++ interface/src/scripting/ScreenshareScriptingInterface.h
@@ -0,0 +1,16 @@
+#ifndef hifi_ScreenshareScriptingInterface_h
+#define hifi_ScreenshareScriptingInterface_h
+
+#include <QObject>
+#include <DependencyManager.h>
+
+class ScreenshareScriptingInterface : public QObject, public Dependency {
+    Q_OBJECT
+public:
+	ScreenshareScriptingInterface();
+    const QString SCREENSHARE_APPLICATION{ "file:///C:/hifi/hifi/screenshare/screenshare-win32-x64/screenshare.exe" };
+
+	Q_INVOKABLE void startScreenshare();
+};
+
+#endif // hifi_ScreenshareScriptingInterface_h
