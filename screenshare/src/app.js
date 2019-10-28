5a3a900cffc889c299878378adffdeef2cab6378
milad <milad@miladnazeri.com>
2019-10-28 09:00:57 -0700

    fixing browser/electronUI

--- screenshare/src/app.js
+++ screenshare/src/app.js
@@ -5,35 +5,130 @@
         }
     }
 
+var images = 10;
+var imageWidth = 265;
+var imageHeight = 165;
+var testImage = "resources/test.jpg";
+function MakeSource(newTitle, newImage, newFakeID, newWidth, newHeight){
+    this.title = newTitle;
+    this.image = newImage;
+    this.fakeID = newFakeID;
+    this.width = newWidth;
+    this.height = newHeight;
+}
+
+let testSources = [];
+
+for (let index = 0; index < images; index++) {
+    let test = new MakeSource("title" + index, testImage, index, imageWidth, imageHeight);
+    testSources.push(test);
+}
+console.log(testSources);
+
+let currentScreensharePick = "";
+function findPick(pick){
+    currentScreensharePick = pick;
+    togglePage();
+}
+
+
+confirmedID = "";
+function screenConfirmed(isConfirmed){
+    if (isConfirmed === true){
+    
+    }
+    togglePage();
+}
+
+let currentPage = "mainPage";
+function togglePage(){
+    if (currentPage === "mainPage") {
+        currentPage = "confirmationPage";
+        document.getElementById("select_screen").style.display = "none";
+        document.getElementById("confirmation_screen").style.display = "block";
+    } else {
+        currentPage = "mainPage";
+        document.getElementById("select_screen").style.display = "block";
+        document.getElementById("confirmation_screen").style.display = "none";
+    }
+}
 
 // UI
     // TODO:
     function addSource(source) {
-        // let screenLabel = `<option value="${source.id.replace(":", "")}" text="${source.name}">${source.name}</option>`;
-        // $('select').append($(stringText));
-        // $(`select option[value="${source.id.replace(":", "")}"]`).attr('data-img-src', source.thumbnail.toDataURL());
+        console.log(source);
+        let title = source.title; 
+        console
+        let imageToTry = source.image
+        console.log(imageToTry)
+        let fakeID = source.fakeID;
+        let width = source.width;
+        let height = source.height;
+        let sourceBody = document.createElement('div')
+        sourceBody.classList.add("box")
+        sourceBody.innerHTML = `
+        <div class="heading">
+            <div class="circle" onClick="togglePage()"></div>
+            <span class="screen_label">${title}</span>
+        </div>
+        <div class="image">
+            <img src="${imageToTry}" width="${width}" height="${height}" data-id="${fakeID}"></img>
+        </div>
+        `
+        document.getElementById("selects").appendChild(sourceBody);
+        console.log(selects);
     }
 
-
+/* BROWSER
+        function showSources() {
+        document.getElementById("selects").innerHTML="";
+        for (let source of testSources) {
+            addSource(source)
+        }
+*/
+    // /* ELECTRON
     let chromeID;
     const electron = require('electron');
     const {desktopCapturer} = electon;
     function showSources() {
+        document.getElementById("selects").innerHTML="";
         desktopCapturer.getSources({ types:['window', 'screen'] }, (error, sources) => {
             if (error) {
                 console.log("Error getting sources", error);
             }
 
-            for (let source of sources) {
-                console.log("Name: " + source.name);
+            for (let source of sources) { 
+                console.log("Name: " + source);
                 addSource(source);
                 if (source.name.indexOf("Google Chrome") > -1){
                     chromeID = source.id;
                 }
-            }
+
+
         });
     }
+    // */
+    function showSources() {
+        document.getElementById("selects").innerHTML="";
+        // console.log(sources);
+        for (let source of testSources) {
+            addSource(source)
+        }
+        // desktopCapturer.getSources({ types:['window', 'screen'] }, (error, sources) => {
+            // if (error) {
+                // console.log("Error getting sources", error);
+            // }
 
+            // for (let source of testSources) { 
+                // console.log("Name: " + source.name);
+                // addSource(source);
+                // if (source.name.indexOf("Google Chrome") > -1){
+                //     chromeID = source.id;
+                // }
+
+
+        // });
+    }
 
     let desktopSharing = false;
     let localStream;
@@ -54,7 +149,7 @@
 
             document.getElementById('screenshare').innerHTML = "Start Screenshare";
             stopTokBoxPublisher();
-            showSources();
+            // showSources();
         }
     }
 
@@ -149,20 +244,20 @@
 
   
 // main TODO:
-    const {ipcRenderer} = ipcRenderer;
-    let apiKey;
-    let sessionId;
-    let token;
-    let session;
-    ipcRenderer.on('connectionInfo', function(event, message){
-        console.log("event", event);
-        console.log("MESSAGE FROM MAIN", message);
-        const connectionInfo = JSON.parse(message); 
+    // const {ipcRenderer} = ipcRenderer;
+    // let apiKey;
+    // let sessionId;
+    // let token;
+    // let session;
+    // ipcRenderer.on('connectionInfo', function(event, message){
+    //     console.log("event", event);
+    //     console.log("MESSAGE FROM MAIN", message);
+    //     const connectionInfo = JSON.parse(message); 
         // apiKey = connectionInfo.apiKey;
         // sessionId = connectionInfo.sessionId;
         // token = connectionInfo.token;
         // initializeTokboxSession();
-    })
+    // })
 
     function startup(){
         console.log("\n\n IN STARTUP \n\n") 
@@ -177,16 +272,19 @@
                 sessionId = json.sessionId;
                 token = json.token;
 
-                initializeTokboxSession();
+                // initializeTokboxSession();
             })
             .catch(function catchErr(error) {
                 handleError(error);
                 alert('Failed to get opentok sessionId and token. Make sure you have updated the config.js file.');
             });
 
-        document.addEventListener("DOMContentLoaded", () => {
-            showSources();
-        })
+
     }
 
-    startup();
+    // startup();
+    document.addEventListener("DOMContentLoaded", () => {
+        showSources();
+
+        // showSources();
+    })
